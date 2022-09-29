f6c2e171713c585b6eaea0d3256a4f50c2e04ec2
dinklang <sdinklang@gmail.com>
2022-09-29 01:23:09 -0500

    change css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,26 @@
 import React, {useState} from 'react'
 import App from "../../App"
 import {Link} from 'react-router-dom'
+import './Navbar.css'
 
 function Navbar() {
     return (
         <>
-            <nav className="navbar">
-                <div className="navbar-container">
-                    <Link to="/">Home</Link>
-                    <Link to="/">About</Link>
+            <nav className="pog-navbar">
+                <div className="pog-navbar__container">
+                    <div className="pog-navbar__container--logo">Sandro</div>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/">About</Link>
+                    </div>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/">About</Link>
+                    </div>
+                    <div>
+                        <Link to="/">Home</Link>
+                        <Link to="/">About</Link>
+                    </div>
                 </div>
             </nav>
         </>
