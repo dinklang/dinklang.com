93eb0f1c7fedb376e934c4253aae594562ae66bc
dinklang <sdinklang@gmail.com>
2022-10-03 01:49:55 -0500

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header__container">
+        <div className="header__title">
+          <p>hi, i'm sandro</p>
+        </div>
+        <div className="header__message">
+          <p>im a peruvian legend</p>
+        </div>
+        <div className="header__description">
+          <p>i'm a software engineer based in Dallas, Texas. i like to bring good ideas to life whether it is in business, music, or programming.</p>
+        </div>
+    </div>
   )
 }
 
