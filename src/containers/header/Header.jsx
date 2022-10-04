600bb7aa5e347abddae13a4f3ac18861493b79b1
dinklang <sdinklang@gmail.com>
2022-10-04 01:35:15 -0500

    updt

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,11 +4,12 @@ import './header.css'
 const Header = () => {
   return (
     <div className="header__container">
-        <div className="header__title">
-          <p>hi, i'm sandro</p>
-        </div>
+        <span className="header__title">
+          hi, i'm
+          <span className="pog-name"> sandro</span>
+        </span>
         <div className="header__message">
-          <p>im a peruvian legend</p>
+          <p>i'm a peruvian legend</p>
         </div>
         <div className="header__description">
           <p>i'm a software engineer based in Dallas, Texas. i like to bring good ideas to life whether it is in business, music, or programming.</p>
