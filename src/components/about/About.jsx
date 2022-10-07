5ebe3d39ce4a05ed2b9ec69e7aabad9723bd5e79
dinklang <sdinklang@gmail.com>
2022-10-07 15:14:44 -0500

    up

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,21 +1,22 @@
 import React from 'react'
 import './about.css'
-import FadeInSection from "../FadeInSection";
 
-function Experience() {
-  return (
-    <div className="about__container">
-      <div className="about__title"><p>about me</p></div>
-      <div className="about__me">
-        i am currently a computer science student at the University of Texas - Arlington,
+class About extends React.Component {
+  render() {
+    return (
+      <div className="about__container">
+        <div className="about__title"><p>about me</p></div>
+        <div className="about__me">
+          i am currently a computer science student at the University of Texas - Arlington,
+        </div>
+
+        <div className="about__technologies">
+          here are some of the technologies i've been working with:
+        </div>
+
       </div>
-
-      <div className="about__technologies">
-        here are some of the technologies i've been working with:
-      </div>
-
-    </div>
-  )
+    )
+  }
 }
 
-export default Experience
+export default About
