11b9cc8b7c770ed16cb56989754141ff3364a445
dinklang <sdinklang@gmail.com>
2022-11-02 21:00:20 -0500

    updt

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,34 +5,46 @@ import FadeInSection from "../FadeInSection";
 class About extends React.Component {
 
   render() {
-    const one = (<p>i am currently a computer science student at the University of Texas - Arlington, i am passionate for many things."</p>);
+    const one = (
+      <p>i am currently a computer science student at the
+        <a href='https://www.uta.edu/'> University of Texas at Arlington</a>
+        . i've been working at
+        <a href='https://www.amazon.com/'> Amazon </a>
+        through college and have a background in IT.<br></br>
+        here are some of the technologies i've been working with:</p>
+    );
+
+    const two = (
+      <p>outside of work i enjoy music production, ocassional video games
+        and the pursue entrepreneurial activities.
+      </p>
+    )
 
     const tech_list = [
       "Javascript",
       "Python",
-      "React.js",
-      "Java",
-      "HTML & CSS",
+      "React",
+      "GraphQL",
       "C++"
     ];
 
     return (
-      <div className="about__container">
+      <div className='about__container'>
         <div className="about__title"><p>about me</p></div>
         <div className="about__me">
-          {[one]}
-        </div>
-        <div className="about__technologies">
-          <p>here are some of the technologies i've been working with:</p>
-          <ul className="tech-stack">
-            {tech_list.map(function (tech_item, i) {
-              return (
-                <FadeInSection delay={`${i + 1}00ms`}>
-                  <li>{tech_item}</li>
-                </FadeInSection>
-              );
-            })}
-          </ul>
+          <div>{[one]}</div>
+          <div className="about__technologies">
+            <ul className="tech-stack">
+              {tech_list.map(function (tech_item, i) {
+                return (
+                  <FadeInSection delay={`${i + 1}00ms`}>
+                    <li>{tech_item}</li>
+                  </FadeInSection>
+                );
+              })}
+            </ul>
+            </div>
+          <div>{[two]}</div>
         </div>
 
       </div>
--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import './cta.css';
+import ValidationTextFields from './FormPropsTextField';
 
 const CTA = () => {
   return (
-    <div>CTA</div>
+    <div className='cta_section'>
+      <div className='cta_title'><p>contact me</p></div>
+      <ValidationTextFields></ValidationTextFields>
+    </div>
   )
 }
 
